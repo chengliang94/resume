41c9d68678e3b81104e2ac41892964f71809253b
chengliang94 <791571782@qq.com>
2016-12-26 08:55:34 +0800

    Recume
    personal resume

---
+++ src/scripts/index.js
@@ -0,0 +1,35 @@
+var $ = require('./common/libs/zepto-modules/zepto');
+require('./common/libs/zepto-modules/event');
+require('./common/libs/zepto-modules/ajax');
+
+var Swiper = require('./common/libs/swiper/swiper.min.js');
+var swiperAni = require('./common/libs/swiper/swiper.animate1.0.2.min.js');
+var IScroll = require('./common/libs/iscroll/iscroll.js');
+
+// edit index
+$(".swiper-container").show();
+$("#mainContainer").hide();
+
+var swiper = new Swiper('.swiper-container',{
+  onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
+    swiperAni.swiperAnimateCache(swiper); //隐藏动画元素 
+    swiperAni.swiperAnimate(swiper); //初始化完成开始动画
+  }, 
+  onSlideChangeEnd: function(swiper){ 
+    swiperAni.swiperAnimate(swiper); //每个slide切换结束时也运行当前slide动画
+  } 
+});
+
+$("#page_four .detail_p").click(function(){
+	$(".swiper-container").hide();
+	$("#mainContainer").show();
+	
+
+	$.post('http://localhost:8000/skill',function(data){
+		console.log(data);
+		var myScroll;
+		myScroll = new IScroll('#wrapper', { mouseWheel: true });
+		document.addEventListener('touchmove', function (e) { e.preventDefault(); }, false);
+	})	
+})
+
